86f88a679dc3d245c63ab94a6f3ab007dc1b14d4
Hüseyin Daş <73713281+huseyindas@users.noreply.github.com>
2024-08-20 14:38:15 +0300

    add base llm func

---
+++ actions/actions.py
@@ -0,0 +1,20 @@
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import UserUtteranceReverted
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionLLM(Action):
+    def name(self) -> Text:
+        return "action_llm"
+
+    async def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message("LLM action test meessage!")
+
+        return [UserUtteranceReverted()]
