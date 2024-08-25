476d19c8757a45306833351ee879273a95222bc3
huseyindas <hsyndass@gmail.com>
2024-08-26 02:51:23 +0300

    deleted old llm-assistant version

--- llm_assistant/ai/storage.py
+++
@@ -1,9 +0,0 @@
-from phi.storage.assistant.postgres import PgAssistantStorage
-
-from .consts import VECTOR_DB
-
-
-combined_assistant_storage = PgAssistantStorage(
-    db_url=VECTOR_DB,
-    table_name="combined_assistant",
-)
--- llm_assistant/api/consts.py
+++
@@ -1,5 +0,0 @@
-import os
-
-VECTOR_DB = os.getenv("VECTOR_DB")
-OLLAMA_HOST = os.getenv("OLLAMA_HOST")
-OLLAMA_MODEL = os.getenv("OLLAMA_MODEL")
